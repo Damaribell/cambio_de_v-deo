0f8af64b3d7ac7916f38b020d3aff40296b9639a
Damaribell <damaris.gimenez23@gmail.com>
2024-07-12 22:11:09 -0400

    Add files via upload

---
+++ javascripts/ajax_sample.js
@@ -0,0 +1,40 @@
+let number = 0;
+let data = []; 
+const button = document.getElementById('btn');
+const titleArea = document.getElementById("title");
+const contentArea = document.getElementById("content");
+const videoArea = document.getElementById("video");
+
+function getData() {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+      data = request.response; 
+      updateContent(); 
+    }
+  };
+  request.open("GET", "ajax.json");
+  request.responseType = "json";
+  request.send();
+}
+
+function updateContent() {
+  if (data.length > 0) {
+    titleArea.innerHTML = data[number].title;
+    contentArea.innerHTML = data[number].content;
+    videoArea.setAttribute("src", data[number].url);
+    number = (number + 1) % data.length; 
+  }
+}
+
+function changeVideo() {
+  if (data.length === 0) {
+    getData(); 
+  } else {
+    updateContent(); 
+  }
+}
+
+button.addEventListener('click', changeVideo);
+
+window.onload = changeVideo;
